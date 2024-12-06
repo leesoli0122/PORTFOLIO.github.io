027413dbf80c7d6f8ad97e460d52a65fa941a1ae
leesoli0122 <leesoli0122@gmail.com>
2024-12-06 09:38:23 +0900

    [위치 변경]

--- progressive/4강/vue/src/components/HelloWorld.vue
+++ progressive/4강/vue/src/components/HelloWorld.vue
@@ -2,6 +2,7 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <h2>안녕하세요!</h2>
+    <button @click="increment">Counter: {{ counter }}</button>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -36,7 +37,20 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
-  }
+  },
+  data() {
+		return {
+			counter: 0,
+		};
+	},
+	methods: {
+		increment() {
+			this.counter++;
+		},
+	},
+	mounted() {
+		console.log('애플리케이션이 마운트 되었습니다!');
+	},
 }
 </script>
 
